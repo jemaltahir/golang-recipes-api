5c976c4505d5aaf60a6693871e277291b2122a0e
Tahir Jemal <jemalmtahir@gmail.com>
2023-02-18 15:53:44 +0200

    new recipe endpoint

--- main.go
+++ main.go
@@ -2,18 +2,42 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
+	"net/http"
 	"time"
 )
 
-func main() {
-	router := gin.Default()
-	router.Run(":8080")
-}
-
 type Recipe struct {
+	ID           string    `json:"id"`
 	Name         string    `json:"name"`
 	Tags         []string  `json:"tags"`
 	Ingredients  []string  `json:"ingredients"`
 	Instructions []string  `json:"instructions"`
 	PublishedAt  time.Time `json:"published_at"`
+}
+
+var recipes []Recipe
+
+func init() {
+	recipes = make([]Recipe, 0)
+}
+func NewRecipeHandler(c *gin.Context) {
+	var recipe Recipe
+	if err := c.BindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	c.JSON(200, gin.H{"recipe": recipe})
+}
+func main() {
+	router := gin.Default()
+	router.POST("/recipes", NewRecipeHandler)
+
+	err := router.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
 }
